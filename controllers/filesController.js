c6e85a1776818257aa819911972216db3906129b
Muhlifain <firganamuhlifain@gmail.com>
2023-04-14 23:06:41 +0700

    add base64 example

--- controllers/filesController.js
+++ controllers/filesController.js
@@ -1,7 +1,15 @@
+var fs = require("fs");
+
 exports.fileUpload = async (req, res, next) => {
     try {
         console.log(req.file);
         console.log(req.body);
+        const base64File = fs.readFileSync(req.file.path, { encoding: 'base64' });
+        //fileName, base64File <--- input to database
+        fs.unlinkSync(req.file.path);
+        /**
+         * img src={file.base64File}
+         */
     } catch (error) {
         next(error);
     }
